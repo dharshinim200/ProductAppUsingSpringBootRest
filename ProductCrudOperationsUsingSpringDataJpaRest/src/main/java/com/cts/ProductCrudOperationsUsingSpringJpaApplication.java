813287c0c587d79c943302685a90b3968ade1b2c
dharshinimurugan <dharshinimgn@gmail.com>
2025-04-23 10:04:17 +0530

    day 1 task completed

--- ProductCrudOperationsUsingSpringDataJpaRest/src/main/java/com/cts/ProductCrudOperationsUsingSpringJpaApplication.java
+++ ProductCrudOperationsUsingSpringDataJpaRest/src/main/java/com/cts/ProductCrudOperationsUsingSpringJpaApplication.java
@@ -15,6 +15,7 @@ import com.cts.service.ProductServiceImpl;
 public class ProductCrudOperationsUsingSpringJpaApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hello");
 		SpringApplication.run(ProductCrudOperationsUsingSpringJpaApplication.class, args);
 }
 }
